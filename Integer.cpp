ad674845cf84527002b466613463e215807c40f8
Mikhail Shushkovsky <63235817+FANDIM-wolf@users.noreply.github.com>
2022-11-11 20:52:08 +0300

    Add files via upload

---
+++ Integer.cpp
@@ -0,0 +1,20 @@
+/*
+The class describes data type int
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Integer {
+public:
+    string name;
+    int value;
+    Integer(string name, int value) {
+        name = name;
+        value = value;
+    }
+
+
+};
