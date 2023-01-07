68143b6b986f8aa9770cb5aaa69003bf24b987d9
Mikhail Shushkovsky <63235817+FANDIM-wolf@users.noreply.github.com>
2023-01-08 01:28:07 +0300

    Add files via upload

--- src/lib/Bool_operations.h
+++ src/lib/Bool_operations.h
@@ -4,40 +4,40 @@
 #include <cmath>
 using namespace std;
 
-bool is_equal(int first_variable, int second_variable, bool condition_to_work) {
+int is_equal(int first_variable, int second_variable, int condition_to_work) {
 
     if (first_variable == second_variable) {
-        return true;
+        return 1;
 
     }
     else {
-        return false;
+        return 0;
     }
 }
 
-bool is_not_equal(int first_variable, int second_variable, bool condition_to_work) {
+int is_not_equal(int first_variable, int second_variable, int condition_to_work) {
 
     if (first_variable != second_variable) {
-        return true;
+        return 1;
 
     }
     else {
-        return false;
+        return 0;
     }
 }
 
-bool is_less(int first_variable, int second_variable, bool condition_to_work) {
+int is_less(int first_variable, int second_variable, int condition_to_work){
 
     if (first_variable < second_variable) {
-        return true;
+        return 1;
 
     }
     else {
-        return false;
+        return 0;
     }
 }
 
-int is_more(int first_variable, int second_variable, bool condition_to_work) {
+int is_more(int first_variable, int second_variable, int condition_to_work) {
 
     if (first_variable > second_variable) {
         return 1;
@@ -52,27 +52,43 @@ int is_more(int first_variable, int second_variable, bool condition_to_work) {
 
 int is_condition_work(int first_variable, int second_variable, string bool_sign, int condition_to_work) {
     int result, result_of_expression;
-
+    result = 0;
+    
     if (bool_sign == ">") {
         result = is_more(first_variable, second_variable, condition_to_work);
+        if (result == condition_to_work) {
+      
+            return result;
+        }
         //return result;
     }
     if (bool_sign == "<") {
         result = is_less(first_variable, second_variable, condition_to_work);
+        if (result == condition_to_work) {
+            
+            return result;
+        }
         //return result;
     }
     if (bool_sign == "!=") {
         result = is_not_equal(first_variable, second_variable, condition_to_work);
+        if (result == condition_to_work) {
+            
+            return result;
+        }
         //return result;
     }
     if (bool_sign == "==") {
         result = is_equal(first_variable, second_variable, condition_to_work);
+        cout << "r!" << endl;
+        if (result == condition_to_work) {
+            
+            return result;
+        }
         //return result;
     }
 
-    if (result == condition_to_work) {
-        return 1;
-    }
+    
 
     return 0;
 
