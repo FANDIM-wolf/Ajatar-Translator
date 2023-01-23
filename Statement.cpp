c8658713e74cda9928ba00ed8afb1410808e6bb3
Mikhail Shushkovsky <63235817+FANDIM-wolf@users.noreply.github.com>
2023-01-23 22:47:10 +0300

    Add files via upload

--- Statement.cpp
+++ Statement.cpp
@@ -1,5 +1,5 @@
 /*
-Class "Statement" describes work of statements such like "if" and "while"
+Class "Statement" describes work of statements such like "if"
 
 */
 
@@ -14,13 +14,19 @@ using namespace std;
 class Statement {
 public:
 
-	int pk; //to get access to object
+	int pk; //to get access for object
 	
 	int type_of_statement;
-	void edit_information_about_current_object(int pk , int type_of_statement ) {
+	int status;
+	int start_line;
+	string statement;
+	void edit_information_about_current_object(int pk , int type_of_statement , int status  , int start_line , string statement) {
 	
 		this->pk = pk;
 		this->type_of_statement = type_of_statement;
+		this->status = status;
+		this->start_line = start_line;
+		this->statement = statement;
 	}
 
 };
